07202f746b84aaae2f47fe81d45e33f1880e9053
Sergey Strukov <sshimnick@hotmail.com>
2017-10-10 00:45:44 +0300

    update

--- Applied/CCore/inc/math/FFTMul.h
+++ Applied/CCore/inc/math/FFTMul.h
@@ -36,17 +36,117 @@ struct FFTMul
 
   static constexpr unsigned UnitBits = Algo::UnitBits ;
 
-   //
-   // d >= 2
-   // N = 2^d
-   // K > 0
-   // 2*K+d <= N
-   //
+  // Unit functions
+
+  static Unit Bit(unsigned s) { return Unit(1)<<s; }
+
+  static Unit Mask(unsigned s) { return Bit(s)-1; }
+
+  struct BitLen
+   {
+    ulen len;
+    unsigned bits; // < UnitBits
+
+    explicit BitLen(ulen len_,unsigned bits_=0) : len(len_),bits(bits_) {}
+
+    bool operator + () const { return len || bits ; }
+
+    bool operator > (BitLen obj) const
+     {
+      if( len>obj.len ) return true;
+
+      if( len<obj.len ) return false;
+
+      return bits>obj.bits;
+     }
+
+    BitLen operator - (BitLen obj) const
+     {
+      if( bits>=obj.bits )
+        return BitLen(len-obj.len,bits-obj.bits);
+      else
+        return BitLen(len-obj.len-1,UnitBits+bits-obj.bits);
+     }
+
+    static BitLen OfBits(ulen blen)
+     {
+      return BitLen(blen/UnitBits,blen%UnitBits);
+     }
+   };
+
+  struct BitPtr
+   {
+    const Unit *ptr;
+    unsigned off; // < UnitBits
+
+    explicit BitPtr(const Unit *ptr_,unsigned off_=0) : ptr(ptr_),off(off_) {}
+
+    BitPtr operator + (BitLen delta) const
+     {
+      const Unit *p=ptr+delta.len;
+      unsigned o=off+delta.bits;
+
+      if( o<UnitBits )
+        return BitPtr(p,o);
+      else
+        return BitPtr(p+1,o-UnitBits);
+     }
+
+    static Unit MaskBit(Unit a,unsigned bits)
+     {
+      return a&Mask(bits);
+     }
+
+    void fill(Unit *A,ulen T,BitLen blen) const
+     {
+      if( off )
+        {
+         blen=blen+BitLen(0,off);
+
+         Algo::Copy(A,ptr,blen.len);
+
+         if( blen.bits )
+           {
+            A[blen.len]=MaskBits(ptr[blen.len],blen.bits);
+
+            Algo::URShift(A,blen.len+1,off);
+
+            Algo::Null(A+blen.len+1,T-blen.len-1);
+           }
+         else
+           {
+            Algo::URShift(A,blen.len,off);
+
+            Algo::Null(A+blen.len,T-blen.len);
+           }
+        }
+      else
+        {
+         Algo::Copy(A,ptr,blen.len);
+
+         if( blen.bits )
+           {
+            A[blen.len]=MaskBits(ptr[blen.len],blen.bits);
+
+            Algo::Null(A+blen.len+1,T-blen.len-1);
+           }
+         else
+           {
+            Algo::Null(A+blen.len,T-blen.len);
+           }
+        }
+     }
+   };
 
   // private functions
 
    //
-   // T = ceil( (N+1)/UnitBits )
+   // d >= 2
+   // N = 2^d , N > UnitBits
+   // K > 0
+   // 2*K+d <= N
+   //
+   // T = ceil( (N+1)/UnitBits ) , T*UnitBits >= N+1
    //
 
   static ulen GetT(ulen N)
@@ -54,34 +154,162 @@ struct FFTMul
     return N/UnitBits+1;
    }
 
-  static void Norm(ulen N,ulen T,Unit *A) // TODO
+  static Unit UAdd(Unit *restrict b,ulen nb,const Unit *a,ulen na) // nb>=na
    {
+    Unit carry=Algo::UAdd(b,a,na);
+
+    return Algo::UAddUnit(b+na,nb-na,carry);
    }
 
-  static void AddC(ulen N,ulen T,Unit *A) // TODO
+  static Unit USub(Unit *restrict b,ulen nb,const Unit *a,ulen na) // nb>=na
    {
+    Unit borrow=Algo::USub(b,a,na);
+
+    return Algo::USubUnit(b+na,nb-na,borrow);
    }
 
-  static void SubC(ulen N,ulen T,Unit *A) // TODO
+
+  static void Norm(ulen N,ulen T,Unit *A)
    {
+    if( unsigned s=unsigned( N%UnitBits ) )
+      {
+       // never happens in practise, cannot be tested
+
+       Unit b=(A[T-1]>>s);
+
+       A[T-1]&=Mask(s);
+
+       if( Algo::USubUnit(A,T,b) ) SubC(N,T,A);
+      }
+    else
+      {
+       Unit b=A[T-1];
+
+       A[T-1]=0;
+
+       if( Algo::USubUnit(A,T,b) ) SubC(N,T,A);
+      }
    }
 
-  static void ModMul(ulen N,ulen T,Unit *A,const Unit *B,Unit *temp) // TODO
+  static void Mod(ulen N,ulen T,Unit *A,const Unit *P) // TODO
    {
+    if( unsigned s=unsigned( N%UnitBits ) )
+      {
+       // never happens in practise, cannot be tested
+
+       // TODO
+      }
+    else
+      {
+       Algo::Copy(A,P,T-1);
+
+       Unit c=UAdd(A,T-1,P+2*(T-1),2);
+       Unit b=Algo::USub(A,P+(T-1),T-1);
+
+       A[T-1]=c-b;
+
+       if( c<b ) SubC(N,T,A);
+      }
    }
 
-  static void ModLShift(ulen N,ulen T,Unit *A,ulen s,Unit *temp) // s<N // TODO
+  static void AddC(ulen N,ulen T,Unit *A)
+   {
+    if( unsigned s=unsigned( N%UnitBits ) )
+      {
+       // never happens in practise, cannot be tested
+
+       Unit b=(A[T-1]>>s)|Bit(UnitBits-s);
+
+       A[T-1]&=Mask(s);
+
+       if( Algo::USubUnit(A,T,b) ) SubC(N,T,A);
+      }
+    else
+      {
+       Unit b[2]={A[T-1],1};
+
+       A[T-1]=0;
+
+       if( USub(A,T,b,2) ) SubC(N,T,A);
+      }
+   }
+
+  static void SubC(ulen N,ulen T,Unit *A)
+   {
+    if( unsigned s=unsigned( N%UnitBits ) )
+      {
+       // never happens in practise, cannot be tested
+
+       Unit b=Bit(UnitBits-s)-(A[T-1]>>s);
+
+       A[T-1]&=Mask(s);
+
+       Algo::UAddUnit(A,T,b);
+      }
+    else
+      {
+       if( Unit u=A[T-1] )
+         {
+          Algo::UAddUnit(A,T,UIntNeg<Unit>(u));
+         }
+       else
+         {
+          Algo::UAddUnit(A+1,T-1,1);
+         }
+      }
+   }
+
+
+  static void ModMul(ulen N,ulen T,Unit *A,const Unit *B,Unit *temp)
+   {
+    Unit *P=temp;
+    Unit *t=P+2*T;
+
+    Algo::UMul(P,A,B,T,t);
+
+    Mod(N,T,A,P);
+   }
+
+  static void ModLShift(ulen N,ulen T,Unit *A,ulen s,Unit *temp) // s<N
    {
     if( s==0 ) return;
 
+    Unit *P=temp;
 
+    ulen delta=s/UnitBits;
+    unsigned shift=unsigned( s%UnitBits );
+
+    Algo::Null(P,delta);
+
+    Unit *a=P+delta;
+
+    Algo::Copy(a,A,T);
+
+    if( shift )
+      {
+       a[T]=Algo::ULShift(a,T,shift);
+
+       Algo::Null(a+T+1,T-delta-1);
+      }
+    else
+      {
+       Algo::Null(a+T,T-delta);
+      }
+
+    Mod(N,T,A,P);
    }
 
-  static void ModRShift(ulen N,ulen T,Unit *A,ulen s,Unit *temp) // s<N // TODO
+  static void ModRShift(ulen N,ulen T,Unit *A,ulen s,Unit *temp) // s<N
    {
     if( s==0 ) return;
 
+    ModLShift(N,T,A,N-s,temp);
+    ModNeg(N,T,A);
+   }
 
+  static void ModNeg(ulen N,ulen T,Unit *A)
+   {
+    if( Algo::UNeg(A,T) ) SubC(N,T,A);
    }
 
   static void ModAdd(ulen N,ulen T,Unit *A,const Unit *B)
@@ -169,15 +397,92 @@ struct FFTMul
       }
    }
 
-  static void Fill(unsigned d,ulen N,ulen K,ulen T,Unit *A,const Unit *a,ulen na) // TODO
+  static void Fill(ulen N,ulen K,ulen T,Unit *A,const Unit *a,ulen na)
    {
+    BitPtr ptr(a);
+    BitLen delta=BitLen::OfBits(K);
+    BitLen len(na);
+
+    while( +len )
+      {
+       if( len>delta )
+         {
+          ptr.fill(A,T,delta);
+
+          A+=T;
+          N--;
+
+          ptr=ptr+delta;
+          len=len-delta;
+         }
+       else
+         {
+          ptr.fill(A,T,len);
+
+          A+=T;
+          N--;
+
+          break;
+         }
+      }
+
+    Algo::Null(A,T*N);
    }
 
-  static void Sum(unsigned d,ulen N,ulen K,ulen T,Unit *c,ulen nc,const Unit *C) // TODO
+  static void Sum(Unit *c,ulen nc,Unit *C,ulen T,BitLen off)
    {
+    if( nc<=off.len ) return;
+
+    c+=off.len;
+    nc-=off.len;
+
+    Unit u;
+
+    if( off.bits )
+      u=Algo::ULShift(C,T,off.bits);
+    else
+      u=0;
+
+    if( nc<=T )
+      {
+       Algo::UAdd(c,C,nc);
+      }
+    else
+      {
+       u+=Algo::UAdd(c,C,T);
+
+       Algo::UAddUnit(c+T,nc-T,u);
+      }
+   }
+
+  static void Sum(ulen N,ulen K,ulen T,Unit *c,ulen nc,Unit *C)
+   {
+    Algo::Null(c,nc);
+
+    Unit *Lim=C+T*(2*N-1);
+
+    ulen T1=T;
+
+    if( (N%UnitBits)==0 ) T1--;
+
+    BitLen off(0);
+    BitLen delta=BitLen::OfBits(K);
+
+    for(; C<Lim ;C+=T)
+      {
+       Sum(c,nc,C,T1,off);
+
+       off=off+delta;
+      }
    }
 
   // functions
+
+
+  static ulen UMulTempLen(unsigned d,ulen N) // TODO
+   {
+    return 0;
+   }
 
    //
    // nab*UnitBits <= K*N
@@ -193,8 +498,8 @@ struct FFTMul
     Unit *B=A+L;
     Unit *t=B+L;
 
-    Fill(d,N,K,T,A,a,nab);
-    Fill(d,N,K,T,B,b,nab);
+    Fill(N,K,T,A,a,nab);
+    Fill(N,K,T,B,b,nab);
 
     DirectFFT(d,N,T,A,t);
     DirectFFT(d,N,T,B,t);
@@ -205,7 +510,7 @@ struct FFTMul
 
     for(ulen off=0; off<L ;off+=T) DivNorm(N,T,A+off,d+1,t);
 
-    Sum(d,N,K,T,c,2*nab,A);
+    Sum(N,K,T,c,2*nab,A);
    }
  };
 
