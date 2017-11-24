0d54f9dcacb5f40cebbd756ccf4a7cc444192d2e
Sergey Strukov <sshimnick@hotmail.com>
2017-11-25 00:20:13 +0300

    update

---
+++ Desktop/Lib/CCore/inc/video/LayoutCombo.h
@@ -0,0 +1,31 @@
+/* LayoutCombo.h */
+//----------------------------------------------------------------------------------------
+//
+//  Project: CCore 3.50
+//
+//  Tag: Desktop
+//
+//  License: Boost Software License - Version 1.0 - August 17th, 2003
+//
+//            see http://www.boost.org/LICENSE_1_0.txt or the local copy
+//
+//  Copyright (c) 2017 Sergey Strukov. All rights reserved.
+//
+//----------------------------------------------------------------------------------------
+
+#ifndef CCore_inc_video_LayoutCombo_h
+#define CCore_inc_video_LayoutCombo_h
+
+#include <CCore/inc/video/Layout.h>
+
+namespace CCore {
+namespace Video {
+
+/* classes */
+
+
+} // namespace Video
+} // namespace CCore
+
+#endif
+
