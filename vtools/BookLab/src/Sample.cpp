fb1130ab842c0830f074c664f543e3cd92bdac81
Sergey Strukov <sshimnick@hotmail.com>
2017-12-12 14:14:57 +0300

    update

---
+++ vtools/BookLab/src/Sample.cpp
@@ -0,0 +1,38 @@
+/* Sample.cpp */
+//----------------------------------------------------------------------------------------
+//
+//  Project: BookLab 1.00
+//
+//  License: Boost Software License - Version 1.0 - August 17th, 2003
+//
+//            see http://www.boost.org/LICENSE_1_0.txt or the local copy
+//
+//  Copyright (c) 2017 Sergey Strukov. All rights reserved.
+//
+//----------------------------------------------------------------------------------------
+
+#include <inc/Sample.h>
+
+namespace App {
+
+/* class SampleWindow */
+
+SampleWindow::SampleWindow(SubWindowHost &host,const Config &cfg_)
+ : SubWindow(host),
+   cfg(cfg_)
+ {
+ }
+
+SampleWindow::~SampleWindow()
+ {
+ }
+
+ // methods
+
+Point SampleWindow::getMinSize() const
+ {
+  return Point(100,100);
+ }
+
+} // namespace App
+
