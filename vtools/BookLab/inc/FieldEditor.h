61b87259ae23805a4d82057fb8112e4f39d8766f
Sergey Strukov <sshimnick@hotmail.com>
2018-11-02 17:54:26 +0300

    update

--- vtools/BookLab/inc/FieldEditor.h
+++ vtools/BookLab/inc/FieldEditor.h
@@ -20,9 +20,115 @@ namespace App {
 
 /* classes */
 
+struct FieldControl;
+
+class FieldBool;
+
+class FieldCoord;
+
+class FieldString;
+
+class FieldULen;
+
+class FieldColor;
+
+class FieldStrength;
+
+class FieldAlign;
+
+class FieldEffect;
+
+class FieldPoint;
+
+class FieldRatio;
+
+
 class FieldWindow;
 
 class FieldFrame;
+
+/* struct FieldControl */
+
+struct FieldControl
+ {
+  virtual void set(bool *def_pad,bool def) = 0 ;
+
+  virtual void noField() = 0 ;
+ };
+
+/* class FieldBool */
+
+class FieldBool : public ComboWindow , public FieldControl
+ {
+  public:
+
+   struct Config
+    {
+     // user
+
+     RefVal<Coord> space_dxy = 10 ;
+
+     CtorRefVal<CheckWindow::ConfigType> check_cfg;
+     CtorRefVal<LabelWindow::ConfigType> lab_cfg;
+
+     // app
+
+     template <class AppPref>
+     Config(const UserPreference &user_pref,const AppPref &app_pref) noexcept
+      {
+       bindUser(user_pref.get(),user_pref.getSmartConfig());
+       bindApp(app_pref.get());
+      }
+
+     template <class Bag,class Proxy>
+     void bindUser(const Bag &bag,Proxy proxy)
+      {
+       space_dxy.bind(bag.space_dxy);
+
+       check_cfg.bind(proxy);
+       lab_cfg.bind(proxy);
+      }
+
+     template <class Bag>
+     void bindApp(const Bag &bag)
+      {
+       Used(bag);
+      }
+    };
+
+   using ConfigType = Config ;
+
+  private:
+
+   const Config &cfg;
+
+   bool * pad = 0 ;
+
+   // subs
+
+   CheckWindow check_true;
+   LabelWindow lab_true;
+
+  public:
+
+   FieldBool(SubWindowHost &host,const Config &cfg);
+
+   virtual ~FieldBool();
+
+   // methods
+
+   Point getMinSize() const;
+
+   void setField(bool *pad);
+
+   virtual void set(bool *def_pad,bool def);
+
+   virtual void noField();
+
+   // drawing
+
+   virtual void layout();
+ };
 
 /* class FieldWindow */
 
@@ -38,10 +144,17 @@ class FieldWindow : public ComboWindow
 
      RefVal<VColor> back = Silver ;
 
+     CtorRefVal<ButtonWindow::ConfigType> btn_cfg;
+     CtorRefVal<CheckWindow::ConfigType> check_cfg;
+     CtorRefVal<LabelWindow::ConfigType> lab_cfg;
+
      // app
+
+     CtorRefVal<FieldBool::ConfigType> field_bool_cfg;
 
      template <class AppPref>
      Config(const UserPreference &user_pref,const AppPref &app_pref) noexcept
+      : field_bool_cfg(user_pref,app_pref)
       {
        bindUser(user_pref.get(),user_pref.getSmartConfig());
        bindApp(app_pref.get());
@@ -50,11 +163,12 @@ class FieldWindow : public ComboWindow
      template <class Bag,class Proxy>
      void bindUser(const Bag &bag,Proxy proxy)
       {
-       Used(proxy);
-
        back.bind(bag.back);
-
        space_dxy.bind(bag.space_dxy);
+
+       btn_cfg.bind(proxy);
+       check_cfg.bind(proxy);
+       lab_cfg.bind(proxy);
       }
 
      template <class Bag>
@@ -71,7 +185,46 @@ class FieldWindow : public ComboWindow
    const Config &cfg;
 
    BookLab::Book &book;
-   BookLab::PadType pad;
+
+   bool * def_pad = 0 ;
+   FieldControl * field_ctrl = 0 ;
+   SubWindow * field = 0 ;
+
+   // subs
+
+   ButtonWindow btn_set;
+   CheckWindow check_def;
+   LabelWindow lab_def;
+
+   // fields
+
+   FieldBool field_bool;
+
+  private:
+
+   void noField();
+
+   void setFieldCtrl(FieldControl *field_ctrl,SubWindow *field,bool withdef);
+
+   template <class W,class T>
+   void setFieldCtrl(W &sub,T *pad);
+
+   template <class W,class T>
+   void setFieldCtrl(W &sub,BookLab::OptDataBase<T> *pad);
+
+
+   template <class T>
+   void setField(T *) { noField(); }
+
+   void setField(bool *pad);
+
+   void setField(BookLab::OptDataBase<bool> *pad);
+
+  private:
+
+   void set_pressed();
+
+   SignalConnector<FieldWindow> connector_set_pressed;
 
   public:
 
@@ -84,10 +237,6 @@ class FieldWindow : public ComboWindow
    Point getMinSize() const;
 
    void setField(BookLab::PadType pad);
-
-   // base
-
-   virtual void open();
 
    // drawing
 
--- vtools/BookLab/src/FieldEditor.cpp
+++ vtools/BookLab/src/FieldEditor.cpp
@@ -17,14 +17,185 @@
 
 namespace App {
 
+/* class FieldBool */
+
+FieldBool::FieldBool(SubWindowHost &host,const Config &cfg_)
+ : ComboWindow(host),
+   cfg(cfg_),
+
+   check_true(wlist,cfg.check_cfg),
+   lab_true(wlist,cfg.lab_cfg,"True"_def)
+ {
+  wlist.insTop(check_true,lab_true);
+ }
+
+FieldBool::~FieldBool()
+ {
+ }
+
+ // methods
+
+Point FieldBool::getMinSize() const
+ {
+  Coord space=+cfg.space_dxy;
+
+  LayToRightCenter lay{Lay(check_true),LayLeft(lab_true)};
+
+  return lay.getMinSize(space);
+ }
+
+void FieldBool::setField(bool *pad_)
+ {
+  pad=pad_;
+
+  if( pad ) check_true.check(*pad);
+ }
+
+void FieldBool::set(bool *def_pad,bool def)
+ {
+  if( def_pad )
+    {
+     *def_pad=def;
+
+     if( !def && pad ) *pad=check_true.isChecked();
+    }
+  else
+    {
+     if( pad ) *pad=check_true.isChecked();
+    }
+ }
+
+void FieldBool::noField()
+ {
+  pad=0;
+ }
+
+ // drawing
+
+void FieldBool::layout()
+ {
+  Coord space=+cfg.space_dxy;
+
+  LayToRightCenter lay{Lay(check_true),LayLeft(lab_true)};
+
+  lay.setPlace(getPane(),space);
+ }
+
 /* class FieldWindow */
+
+void FieldWindow::noField()
+ {
+  if( field_ctrl )
+    {
+     def_pad=0;
+
+     field_ctrl->noField();
+
+     field_ctrl=0;
+
+     wlist.del(field);
+
+     field=0;
+
+     btn_set.disable();
+     check_def.disable();
+     lab_def.disable();
+
+     redraw();
+    }
+ }
+
+void FieldWindow::setFieldCtrl(FieldControl *field_ctrl_,SubWindow *field_,bool withdef)
+ {
+  if( field_ctrl==field_ctrl_ )
+    {
+     btn_set.enable();
+
+     check_def.enable(withdef);
+     lab_def.enable(withdef);
+    }
+  else
+    {
+     if( field_ctrl )
+       {
+        field_ctrl->noField();
+
+        wlist.del(field);
+       }
+
+     field_ctrl=field_ctrl_;
+     field=field_;
+
+     wlist.insBottom(field);
+
+     btn_set.enable();
+
+     check_def.enable(withdef);
+     lab_def.enable(withdef);
+
+     redraw();
+    }
+ }
+
+template <class W,class T>
+void FieldWindow::setFieldCtrl(W &sub,T *pad)
+ {
+  def_pad=0;
+
+  sub.setField(pad);
+
+  setFieldCtrl(&sub,&sub,false);
+ }
+
+template <class W,class T>
+void FieldWindow::setFieldCtrl(W &sub,BookLab::OptDataBase<T> *pad)
+ {
+  def_pad=&pad->def;
+
+  sub.setField(&pad->data);
+
+  setFieldCtrl(&sub,&sub,true);
+ }
+
+void FieldWindow::setField(bool *pad)
+ {
+  setFieldCtrl(field_bool,pad);
+ }
+
+void FieldWindow::setField(BookLab::OptDataBase<bool> *pad)
+ {
+  setFieldCtrl(field_bool,pad);
+ }
+
+void FieldWindow::set_pressed()
+ {
+  if( field_ctrl )
+    {
+     field_ctrl->set(def_pad,check_def.isChecked());
+
+     modified.assert();
+    }
+ }
 
 FieldWindow::FieldWindow(SubWindowHost &host,const Config &cfg_,BookLab::Book &book_)
  : ComboWindow(host),
    cfg(cfg_),
 
-   book(book_)
+   book(book_),
+
+   btn_set(wlist,cfg.btn_cfg,"Set"_def),
+   check_def(wlist,cfg.check_cfg),
+   lab_def(wlist,cfg.lab_cfg,"default"_def),
+
+   field_bool(wlist,cfg.field_bool_cfg),
+
+   connector_set_pressed(this,&FieldWindow::set_pressed,btn_set.pressed)
  {
+  wlist.insTop(btn_set,check_def,lab_def);
+
+  btn_set.disable();
+  check_def.disable();
+  lab_def.disable();
  }
 
 FieldWindow::~FieldWindow()
@@ -35,25 +206,38 @@ FieldWindow::~FieldWindow()
 
 Point FieldWindow::getMinSize() const
  {
-  return Point(100,100);
+  Coord space=+cfg.space_dxy;
+
+  LayToRightCenter lay1{Lay(check_def),LayLeft(lab_def)};
+
+  LaySame lay2{Lay(field_bool)};
+
+  LayToBottom lay{LayLeft(btn_set),lay1,lay2};
+
+  return ExtLay(lay).getMinSize(space);
  }
 
-void FieldWindow::setField(BookLab::PadType pad_)
+void FieldWindow::setField(BookLab::PadType pad)
  {
-  pad=pad_;
- }
-
- // base
-
-void FieldWindow::open()
- {
-  ComboWindow::open();
+  if( +pad )
+    pad.apply( [&] (auto *pad) { setField(pad); } );
+  else
+    noField();
  }
 
  // drawing
 
 void FieldWindow::layout()
  {
+  Coord space=+cfg.space_dxy;
+
+  LayToRightCenter lay1{Lay(check_def),LayLeft(lab_def)};
+
+  LaySame lay2{Lay(field_bool)};
+
+  LayToBottom lay{LayLeft(btn_set),lay1,lay2};
+
+  ExtLay(lay).setPlace(getPane(),space);
  }
 
 void FieldWindow::drawBack(DrawBuf buf,bool) const
@@ -77,9 +261,6 @@ FieldFrame::FieldFrame(Desktop *desktop,const Config &cfg_,BookLab::Book &book,S
 FieldFrame::~FieldFrame()
  {
  }
-
- // methods
-
 
  // base
 
