cdd56f6187a7bb0931c3f9739e0e3823d31b7269
Sergey Strukov <sshimnick@hotmail.com>
2019-04-11 07:50:43 +0300

    FavFrame

---
+++ vtools/WinLab/inc/FavFrame.h
@@ -0,0 +1,30 @@
+/* FavFrame.h */
+//----------------------------------------------------------------------------------------
+//
+//  Project: CCore 3.60
+//
+//  Tag: Desktop
+//
+//  License: Boost Software License - Version 1.0 - August 17th, 2003
+//
+//            see http://www.boost.org/LICENSE_1_0.txt or the local copy
+//
+//  Copyright (c) 2019 Sergey Strukov. All rights reserved.
+//
+//----------------------------------------------------------------------------------------
+
+#ifndef CCore_inc_video_FavFrame_h
+#define CCore_inc_video_FavFrame_h
+
+#include <CCore/inc/video/WindowLib.h>
+
+namespace CCore {
+namespace Video {
+
+/* classes */
+
+} // namespace Video
+} // namespace CCore
+
+#endif
+
