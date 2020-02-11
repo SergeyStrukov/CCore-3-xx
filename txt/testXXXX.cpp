da0286388d2e746964c15b9ece64b78110afd94c
Sergey Strukov <sshimnick@hotmail.com>
2020-02-11 15:50:35 +0300

    update

--- txt/testXXXX.cpp
+++ txt/testXXXX.cpp
@@ -9,7 +9,7 @@
 //
 //            see http://www.boost.org/LICENSE_1_0.txt or the local copy
 //
-//  Copyright (c) 2019 Sergey Strukov. All rights reserved.
+//  Copyright (c) 2020 Sergey Strukov. All rights reserved.
 //
 //----------------------------------------------------------------------------------------
 
